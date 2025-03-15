bf9451364ab7413ac4b1c439476f2dd0ba14eb2d
Jojo5673 <judoudo2005@gmail.com>
2025-03-15 13:31:47 -0500

    Backend Initialized

---
+++ backend/src/main/java/com/webprojects/Intellibus/submission/IntellibusSubmissionApplication.java
@@ -0,0 +1,13 @@
+package com.webprojects.Intellibus.submission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntellibusSubmissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntellibusSubmissionApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/webprojects/Intellibus/submission/IntellibusSubmissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webprojects.Intellibus.submission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntellibusSubmissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
