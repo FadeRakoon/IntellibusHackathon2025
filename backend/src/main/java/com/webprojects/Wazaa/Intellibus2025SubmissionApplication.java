b72ee171b68b67fa13aca8453683899f7b521d75
Jojo5673 <judoudo2005@gmail.com>
2025-03-15 13:50:24 -0500

    Fixed backend for good this time

--- backend/src/main/java/com/webprojects/Wazaa/Intellibus2025SubmissionApplication.java
+++ backend/src/main/java/com/webprojects/Wazaa/Intellibus2025SubmissionApplication.java
@@ -1,13 +1,13 @@
-package com.webprojects.Intellibus.submission;
+package com.webprojects.Wazaa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IntellibusSubmissionApplication {
+public class Intellibus2025SubmissionApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IntellibusSubmissionApplication.class, args);
+		SpringApplication.run(Intellibus2025SubmissionApplication.class, args);
 	}
 
 }
--- backend/src/test/java/com/webprojects/Wazaa/Intellibus2025SubmissionApplicationTests.java
+++ backend/src/test/java/com/webprojects/Wazaa/Intellibus2025SubmissionApplicationTests.java
@@ -1,10 +1,10 @@
-package com.webprojects.Intellibus.submission;
+package com.webprojects.Wazaa;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class IntellibusSubmissionApplicationTests {
+class Intellibus2025SubmissionApplicationTests {
 
 	@Test
 	void contextLoads() {
