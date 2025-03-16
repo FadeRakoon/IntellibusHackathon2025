ad13684524771dbe250105a22e2b77beaeecd769
FadeRakoon <110274955+FadeRakoon@users.noreply.github.com>
2025-03-16 08:19:55 -0500

    orville bun interface things

--- frontend/wazzah_frontend/src/app/page.tsx
+++ frontend/wazzah_frontend/src/app/page.tsx
@@ -1,6 +1,17 @@
-import Image from "next/image";
+"use client"
 
+import Image from "next/image";
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 export default function Home() {
+  const [text, setText] = useState("Wait")
+
+  useEffect(()=>{
+    fetch("http://localhost:3000")
+    .then(t => t.json()
+    .then(text => setText(text.text)));
+
+  })
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
@@ -22,7 +33,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            {text}
           </li>
         </ol>
 
